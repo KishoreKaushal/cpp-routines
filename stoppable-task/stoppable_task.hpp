6fb81482abcc2cf01cda61be43a04f396e595124
KishoreKaushal <kshr4kshl@gmail.com>
2021-03-07 14:34:16 +0530

    status info

--- stoppable-task/stoppable_task.hpp
+++ stoppable-task/stoppable_task.hpp
@@ -3,21 +3,32 @@
 
 #include <future>
 
+enum class TaskState : unsigned char {
+    Ready = 0,
+    Executing = 1,
+    Terminating = 2
+};
+
 class StoppableTask {
     std::promise<void>  exit_signal;
     std::future<void>   future_obj;
 
+protected:
+    TaskState           task_state;
+    virtual void run() = 0;
+
 public:
 
-    StoppableTask() : future_obj(exit_signal.get_future()) {}
+    StoppableTask() 
+    :   future_obj(exit_signal.get_future()), 
+        task_state(TaskState::Ready) {}
 
     StoppableTask(const StoppableTask&) = delete;
     
     StoppableTask& operator=(const StoppableTask&) = delete;
 
-    virtual void run() = 0;
-
     virtual void operator()() {
+        task_state = TaskState::Executing;
         run();
     }
 
@@ -27,7 +38,12 @@ public:
     }
 
     void request_stop() {
+        task_state = TaskState::Terminating;
         exit_signal.set_value();
+    }
+
+    TaskState get_task_state() {
+        return task_state;
     }
 };
 
