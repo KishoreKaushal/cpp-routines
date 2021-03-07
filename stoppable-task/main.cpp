da03e1944822a54c5710f1bb10b4c0a4a76c7eb8
KishoreKaushal <kshr4kshl@gmail.com>
2021-03-07 14:34:21 +0530

    test code

---
+++ stoppable-task/main.cpp
@@ -0,0 +1,31 @@
+#include "stoppable_task.hpp"
+#include <iostream>
+#include <memory>
+
+class Speak : public StoppableTask {
+    int count;
+public:
+    Speak() : count(0), StoppableTask() {}
+
+    Speak(const Speak&) = delete;
+    Speak& operator=(const Speak&) = delete;
+
+    void run() override {
+        std::cout << count << " -- speaking ... " << std::endl;
+    }
+};
+
+int main() {
+    int i = 10;
+
+    std::unique_ptr<Speak> spk = std::make_unique<Speak>();
+
+    do {
+        (*spk)();
+        if (--i <= 0) {
+            spk->request_stop();
+        }
+    } while(not spk->is_stop_requested());
+
+    return 0;
+}
